1ba2448865ef2b87e19b87c5a24e9d94df941bc0
Marcos <messtutors@gmail.com>
2025-01-31 11:47:43 -0300

    atualizando antes de instalar o tailwind

---
+++ gestorDeGrupos/src/App.tsx
@@ -0,0 +1,12 @@
+import { Box } from "@radix-ui/themes";
+import Header from "./components/RootLayout";
+
+
+
+export default function App() {
+  return (
+    <Box>
+      <Header></Header>
+    </Box>
+  )
+}
